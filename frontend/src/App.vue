41999ba8a4573e4c5337dc32d8452deeb7c621cd
matheuscabral <matheuscabralsilva95@gmail.com>
2020-08-12 12:15:25 -0300

    Sistema de login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,50 +1,6 @@
 <template>
   <v-app>
-    <div>
-      <v-app-bar
-        color="primary"
-        dense
-        dark
-      >
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-        <v-toolbar-title>Todos Contra o Covid</v-toolbar-title>
-      </v-app-bar>
-
-      <v-navigation-drawer
-        v-model="drawer"
-        absolute
-        temporary
-      >
-        <v-list
-          nav
-          dense
-        >
-          <v-list-item-group
-            active-class="primary--text text--accent-4"
-          >
-
-            <v-list-item active :to="{name:'Home'}">
-              <v-list-item-icon>
-                <v-icon>mdi-home</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Home</v-list-item-title>
-            </v-list-item>
-
-            <v-list-item :to="{name:'Atendimento'}">
-              <v-list-item-icon>
-                <v-icon>mdi-account</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Atendimento</v-list-item-title>
-            </v-list-item>
-
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
-
-      <v-main>
-        <router-view/>
-      </v-main>
-    </div>
+      <router-view/>
   </v-app>
 </template>
 
@@ -52,12 +8,8 @@
 
 export default {
   name: "App",
-
-  components: {
-  },
-
-  data: () => ({
-    drawer: false
-  })
+  mounted() {
+    this.$router.push("login");
+  }
 };
 </script>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,50 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "../router";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    dadosUser: null,
+    token: localStorage.getItem('token') || ''
+  },
   mutations: {},
-  actions: {},
-  modules: {}
+  actions: {
+    start({ state }) {
+      return new Promise((resolve, reject) => {
+        if(state.token === '')
+          reject();
+
+        Vue.axios.defaults.headers.common['Authorization'] = state.token;
+
+        resolve();
+      })
+    },
+    login({ state }, dadosLogin) {
+      return new Promise((resolve, reject) => {
+        Vue.axios.post("login", dadosLogin)
+        .then(response => {
+          const { access_token, user: { nome, perfil } } = response.data;
+          
+          state.dadosUser = { nome, perfil };
+
+          Vue.axios.defaults.headers.common['Authorization'] = access_token;
+          localStorage.setItem('token', access_token);
+
+          resolve(true);
+        })
+        .catch(error => {
+          localStorage.removeItem('token');
+
+          reject(error);
+        });
+      });
+    }
+  },
+  modules: {},
+  getters: {
+    isLoggedIn: state => !!state.token,
+    getDadosUser: state => state.dadosUser
+  }
 });
