4c846448a5afe7ab9fbe3dd2cfa5f0f0612127e3
matheuscabral <matheus.cabral@connectparts.com.br>
2020-09-02 16:43:31 -0300

    Configurado deploy

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -14,7 +14,7 @@ let config = {
   // baseURL: process.env.baseURL || process.env.apiUrl || ""
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
-  baseURL: "http://localhost:8000/api/",
+  baseURL: process.env.VUE_APP_API_URL || "http://localhost:8000/api/",
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
