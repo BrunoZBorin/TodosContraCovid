079e0d27e1e98d48dd2f3727c4ac9d93b79ea8d6
Bruno Borin <psclaram@gmail.com>
2020-09-04 21:23:07 -0300

    rotas autenticadas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,37 +17,41 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResources([
-    'unidades_saude' => 'API\UnidadeSaudeController',
-    'unidades_sintomaticas' => 'API\UnidadeSintomaticaController',
-    'sinais' => 'API\SinaisController',
-    'atendimentos' => 'API\AtendimentoController',
-    'pacientes' => 'API\PacienteController',
-    'comorbidades' => 'API\ComorbidadesController',
-    'familiares' => 'API\FamiliarController',
-    'users' => 'API\UserController'
-    ]);
-Route::post('/primeiro_cadastro', 'API\PacienteController@primeiro_cadastro');
+Route::group(['middleware' => 'auth:api'], function()
+{
+    Route::apiResources([
+        'unidades_saude' => 'API\UnidadeSaudeController',
+        'unidades_sintomaticas' => 'API\UnidadeSintomaticaController',
+        'sinais' => 'API\SinaisController',
+        'atendimentos' => 'API\AtendimentoController',
+        'pacientes' => 'API\PacienteController',
+        'comorbidades' => 'API\ComorbidadesController',
+        'familiares' => 'API\FamiliarController',
+        'users' => 'API\UserController'
+        ]);
+});
+
+Route::post('/primeiro_cadastro', 'API\PacienteController@primeiro_cadastro')->middleware('auth:api');
 Route::get('/cep', 'API\PacienteController@cep');
 Route::post('/register', 'API\AuthController@register');
 Route::post('/login', 'API\AuthController@login');
-Route::get('/pacientes_export_excel/{id}', 'API\PacienteController@export_excel');
-Route::get('/pacientes_export_pdf/{id}', 'API\PacienteController@export_pdf');
-Route::get('/atendimentos_export_excel/{id}', 'API\AtendimentoController@export_excel');
-Route::get('/atendimentos_export_pdf/{id}', 'API\AtendimentoController@export_pdf');
-Route::get('/index_atendimentos/{id}', 'API\AtendimentoController@index_atendimentos');
-//Graficos
+Route::get('/pacientes_export_excel/{id}', 'API\PacienteController@export_excel')->middleware('auth:api');
+Route::get('/pacientes_export_pdf/{id}', 'API\PacienteController@export_pdf')->middleware('auth:api');
+Route::get('/atendimentos_export_excel/{id}', 'API\AtendimentoController@export_excel')->middleware('auth:api');
+Route::get('/atendimentos_export_pdf/{id}', 'API\AtendimentoController@export_pdf')->middleware('auth:api');
+Route::get('/index_atendimentos/{id}', 'API\AtendimentoController@index_atendimentos')->middleware('auth:api');
+
 Route::get('/atendimentos_por_data', 'API\AtendimentoController@atendimentos_por_data');
 Route::get('/pacientes_obitos', 'API\PacienteController@obitos');
 Route::get('/pacientes_idades', 'API\PacienteController@idades');
 Route::get('/pacientes_com_comorbidades', 'API\PacienteController@pacientes_com_comorbidades');
 Route::get('/casos_finalizados', 'API\PacienteController@casos_finalizados');
 Route::get('/casos_positivos', 'API\PacienteController@casos_positivos');
-//Listagem diaria
-Route::get('/listagem_diaria/{id}', 'API\AtendimentoController@listagem_diaria');
 
-Route::get('/show_sinais_familiares/{id}', 'API\PacienteController@show_sinais_familiares');
-Route::get('/show_atendimento_sinais/{id}', 'API\AtendimentoController@show_atendimento_sinais');
-Route::post('/atendimento_create_paciente_update', 'API\AtendimentoController@store_atendimento_update_paciente');
-Route::post('/usuario_is_admin/{id}', 'API\UserController@is_admin');
-Route::post('/usuario_is_not_admin/{id}', 'API\UserController@is_not_admin');
+Route::get('/listagem_diaria/{id}', 'API\AtendimentoController@listagem_diaria')->middleware('auth:api');
+
+Route::get('/show_sinais_familiares/{id}', 'API\PacienteController@show_sinais_familiares')->middleware('auth:api');
+Route::get('/show_atendimento_sinais/{id}', 'API\AtendimentoController@show_atendimento_sinais')->middleware('auth:api');
+Route::post('/atendimento_create_paciente_update', 'API\AtendimentoController@store_atendimento_update_paciente')->middleware('auth:api');
+Route::post('/usuario_is_admin/{id}', 'API\UserController@is_admin')->middleware('auth:api');
+Route::post('/usuario_is_not_admin/{id}', 'API\UserController@is_not_admin')->middleware('auth:api');
